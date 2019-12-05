78f55fcf3b9600de38b865b390905a372630e9dd
Григорий Пенкин <penkin.g@yandex.ru>
2019-12-05 14:00:41 +0300

    4.review finished

--- javaTask2/src/main/java/hashmap/HashMap.java
+++ javaTask2/src/main/java/hashmap/HashMap.java
@@ -1,6 +1,5 @@
 package hashmap;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 
@@ -11,38 +10,12 @@ public class HashMap<K, V> implements SimpleMap<K, V> {
     private Node<K, V>[] hashList = NewHashListInit(16);
 
     private Node<K, V>[] NewHashListInit(int capacity) {
-        //ArrayList<Node<K, V>> newHashList = new ArrayList<>(capacity); // TODO: Заменить на array
-        //<Node<K, V>[] newHashList  = new <>[];
-
-        //public Array(Class<E> type, int length)
-        //{
-            // Creates a new array with the specified type and length at runtime
-       // newHashList = (Node<K, V>[]) java.lang.reflect.Array.newInstance(Class<?>, capacity);
-
-        //newHashList = new Node<K, V>[capacity];
-
-
-            //public GenSet(Class<E> c, int s) {
-            // Use Array native method to create array
-            // of a type only known at run time
-            //@SuppressWarnings("unchecked")
-            //final E[] a = (E[]) Array.newInstance(c, s);
-
-
-            newHashList = (Node<K, V>[]) Array.newInstance(Node.class, capacity);
-
-
-
-
-
-
-
-
-
+        newHashList = (Node<K, V>[]) new Node[capacity];
 
         for (int i = 0; i < capacity; i++) {
-            newHashList.add(null);
+            newHashList[i] = null;
         }
+
         this.capacity = capacity;
         return newHashList;
     }
@@ -52,9 +25,9 @@ public class HashMap<K, V> implements SimpleMap<K, V> {
 
         if ((float) this.size >= 0.75 * capacity) {
             this.size = 0;
-            ArrayList oldHashList = new ArrayList<>(this.hashList);
+            Node<K, V>[] oldHashList = (Node<K, V>[]) new Node[capacity];
             this.hashList = NewHashListInit(2 * capacity); // capacity изменилось
-            for (int i = 0; i < oldHashList.size(); i++) {
+            for (int i = 0; i < capacity / 2; i++) {
                 NodeIterator nodeIterator = new NodeIterator(oldHashList, i);
                 while (nodeIterator.hasNext()) {
                     node = nodeIterator.next();
@@ -73,7 +46,7 @@ public class HashMap<K, V> implements SimpleMap<K, V> {
             expandList();
             int index = getIndex(key);
             try {
-                Node node = hashList.get(index); //
+                Node node = hashList[index]; //
                 while (true) {
                     if (node.getNextNode() == null) {
                         node.setNextNode(new Node<>(key, value));
@@ -83,7 +56,7 @@ public class HashMap<K, V> implements SimpleMap<K, V> {
                     }
                 }
             } catch (NullPointerException e) {
-                hashList.set(index, new Node<>(key, value));
+                hashList[index] = new Node<>(key, value);
             }
             this.size += 1;
         }
@@ -97,7 +70,7 @@ public class HashMap<K, V> implements SimpleMap<K, V> {
     private Node getNode(K key) {
         int index = getIndex(key);
         try {
-            Node node = hashList.get(index);
+            Node node = hashList[index];
             while (true) {
                 if (node.getKey().equals(key)) {
                     return node;
@@ -136,14 +109,14 @@ public class HashMap<K, V> implements SimpleMap<K, V> {
     @Override
     public V remove(K key) {
         int index = getIndex(key);
-        Node<K, V> node = hashList.get(index);
+        Node<K, V> node = hashList[index];
         if (node == null) {
             //throw new NoSuchElementException("The remove element does not exist");
             return null;
         }
         Node<K, V> nextNode;
         if (node.getKey().equals(key)) {
-            hashList.set(index, null);
+            hashList[index] = null;
             this.size -= 1;
             return node.getValue();
         } else while (true) {
@@ -164,7 +137,7 @@ public class HashMap<K, V> implements SimpleMap<K, V> {
     @Override
     public Set<K> keySet() {
         HashSet<K> set = new HashSet<>();
-        for (int i = 0; i < hashList.size(); i++) {
+        for (int i = 0; i < capacity; i++) {
             NodeIterator nodeIterator = new NodeIterator(this.hashList, i);
             while (nodeIterator.hasNext()) {
                 set.add(nodeIterator.next().getKey());
@@ -176,7 +149,7 @@ public class HashMap<K, V> implements SimpleMap<K, V> {
     @Override
     public Collection<V> values() {
         ArrayList<V> collection = new ArrayList<>();
-        for (int i = 0; i < hashList.size(); i++) {
+        for (int i = 0; i < capacity; i++) {
             NodeIterator nodeIterator = new NodeIterator(this.hashList, i);
             while (nodeIterator.hasNext()) {
                 collection.add(nodeIterator.next().getValue());
@@ -224,13 +197,13 @@ public class HashMap<K, V> implements SimpleMap<K, V> {
 
     class NodeIterator implements Iterator {
         Node<K, V> currentNode;
-        ArrayList<Node<K, V>> hashList;
+        Node<K, V>[] hashList;
 
         // initialize pointer to head of the list for iteration
-        NodeIterator(ArrayList<Node<K, V>> hashList, int index) {
+        NodeIterator(Node<K, V>[] hashList, int index) {
             this.hashList = hashList;
             try {
-                this.currentNode = hashList.get(index);
+                this.currentNode = hashList[index];
             } catch (NullPointerException e) {
                 this.currentNode = null;
             }
