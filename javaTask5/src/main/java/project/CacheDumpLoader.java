72ef3462f70e3280b5e26fda0bb71e869b38c82e
Григорий Пенкин <penkin.g@yandex.ru>
2020-02-14 00:04:11 +0300

    +hanlders

--- javaTask5/src/main/java/project/CacheDumpLoader.java
+++ javaTask5/src/main/java/project/CacheDumpLoader.java
@@ -4,35 +4,44 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class CacheDumpLoader {
+class CacheDumpLoader {
     // Словарь для поиска результата в памяти / на диске
     HashMap<List<Object>, HashMap<String, Object>> resultPath = new HashMap<>();
 
     // Массив результатов. (Если все храниться в памяти) ????
     List<Object> inMemoryResults = new ArrayList<>();
 
-
-    // метод для сохранения в словарь результатов расчета
-    public void dump(List<Object> key, HashMap<String, Object> cacheParams, Object result){
-
-
-
-
-
-
-    }
-
     // метод проверки наличия ключа
-    public boolean containsKey(List<Object> key){
+    boolean containsKey(List<Object> key){
         return resultPath.containsKey(key);
     }
 
+    // метод для сохранения в словарь результатов расчета
+    void dump(List<Object> key, HashMap<String, Object> cacheParams, Object result){
+        // "rootFolder"
+        // "cacheType"
+        // "listSize"
+        // "fileNamePrefix"
+        // "zip"
+        // "identityBy"
+        // TODO: Расположить в нужном порядке
 
 
 
 
+
+
+
+
+
+
+    }
     // метод для извлечения рассчетов
-    public Object load(List<Object> key){
+    Object load(List<Object> key){
+
+
+
+
 
 
 
@@ -43,9 +52,6 @@ public class CacheDumpLoader {
 
         return null;
     }
-
-
-
 }
 
 
