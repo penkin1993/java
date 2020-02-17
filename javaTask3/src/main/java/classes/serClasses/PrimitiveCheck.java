96872068fb94d665e54597a03612f3bbb59a4169
Григорий Пенкин <penkin.g@yandex.ru>
2020-02-17 22:24:14 +0300

    only hookd

--- javaTask3/src/main/java/classes/serClasses/PrimitiveCheck.java
+++ javaTask3/src/main/java/classes/serClasses/PrimitiveCheck.java
@@ -11,17 +11,7 @@ class PrimitiveCheck {
     }
 
     private static Set<Class<?>> getWrapperTypes() {
-        Set<Class<?>> ret = new HashSet<>();
-        ret.add(Boolean.class);
-        ret.add(Character.class);
-        ret.add(Byte.class);
-        ret.add(Short.class);
-        ret.add(Integer.class);
-        ret.add(Long.class);
-        ret.add(Float.class);
-        ret.add(Double.class);
-        ret.add(Void.class);
-        ret.add(String.class);
-        return ret;
+        return Set.of(Boolean.class, Character.class, Byte.class, Short.class, Integer.class,
+                Long.class, Float.class, Double.class, Void.class, String.class);
     }
 }
